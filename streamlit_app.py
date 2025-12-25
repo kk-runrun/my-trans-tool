fa4816e0cb11f5b198983762e231dac8cfbf589e
kk-runrun <qingyiqimen@qq.com>
2025-12-25 16:06:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ def main_app():
         st.session_state.processing = False
 
     FIXED_API_KEY = "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" 
-    FIXED_BASE_URL = "http://gptapi.kuajingvs.com:7999/v1" 
+    FIXED_BASE_URL = "http://gptapi.kuajingvs.com:7999/v1/chat/completions" 
     FIXED_MODEL_NAME = "gpt-4o" 
 
     try:
